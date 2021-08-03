ee0894eaff494f1f16b8d0b4cc8fea5c1455382e
ChiatanyaChakka <73595822+ChiatanyaChakka@users.noreply.github.com>
2021-08-03 20:52:26 +0530

    navIGAtion bar for home screen added.

---
+++ lib/HomePage.dart
@@ -0,0 +1,60 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _currentpage = 0;
+  PageController pageController = PageController();
+  GlobalKey<CurvedNavigationBarState> _navigationkey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: PageView(
+        pageSnapping: false,
+        controller: pageController,
+        children:[
+          Center(child: Text("Home"),),
+          Center(child: Text("List"),),
+          Center(child: Text("Person"),),
+        ],
+        scrollDirection: Axis.horizontal,
+        onPageChanged: (index){
+          setState(() {
+            _currentpage = index;
+            final CurvedNavigationBarState? navigationBarState = _navigationkey.currentState;
+            navigationBarState?.setPage(_currentpage);
+          });
+        },
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.white,
+        key: _navigationkey,
+        items: [
+          Icon(Icons.home, size: 30,),
+          Icon(Icons.list, size: 30,),
+          Icon(Icons.person, size: 30,),
+        ],
+        height: 65,
+        index: _currentpage,
+        buttonBackgroundColor: Colors.blue,
+        backgroundColor: Colors.white,
+        onTap: (index){
+          setState(() {
+            _currentpage = index;
+            pageController.animateToPage(_currentpage, duration: Duration(milliseconds: 200), curve: Curves.easeInOut);
+          });
+        },
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:zeta_book/HomePage.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Homeapp(),
+    home: HomePage(),
   ));
 }
 
-class Homeapp extends StatefulWidget {
-  const Homeapp({Key? key}) : super(key: key);
-
-  @override
-  _HomeappState createState() => _HomeappState();
-}
-
-class _HomeappState extends State<Homeapp> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
