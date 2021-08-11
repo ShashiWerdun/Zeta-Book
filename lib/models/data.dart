6e2ae415794ec6dd424141a4088583a3376ce9bf
shashi <shashivardhan201@gmail.com>
2021-08-11 12:24:04 +0530

    fixed a renderFlex overflow and populated business pages with data.

--- lib/models/data.dart
+++ lib/models/data.dart
@@ -1,16 +1,71 @@
 class Business {
-  String? businessName;
-  String? businessCategory;
+  String name;
+  String category;
+  String phone;
+  String address;
+
+  Business({
+    required this.name,
+    required this.category,
+    required this.phone,
+    required this.address,
+  });
 }
 
-class Customer extends Business {
-  String? customerName;
-  int? customerPhone;
+class Customer {
+  String name;
+  String phone;
+  String address;
+
+  Customer({
+    required this.name,
+    required this.address,
+    required this.phone,
+  });
 }
 
-class Transaction extends Customer {
-  DateTime? dateTime;
-  String? transactionNote;
-  bool? outgoing;
-  int? transactionAmount;
+class Transaction {
+  DateTime timestamp;
+  String note;
+  bool outgoing;
+  double amount;
+
+  Transaction(
+      {required this.timestamp,
+      required this.note,
+      required this.outgoing,
+      required this.amount});
 }
+
+List<Business> businessList = [
+  Business(
+      name: 'Ashok General Store',
+      category: 'Retail Store',
+      phone: '1234567890',
+      address: 'Hyderguda, Hyderabad, Telangana'),
+  Business(
+      name: 'Z Gen Electronics',
+      category: 'Electronics Store',
+      phone: '1234567890',
+      address: 'Abids, Hyderabad, Telangana'),
+  Business(
+      name: 'Chaitanya Startup',
+      category: 'New Startup',
+      phone: '1234567890',
+      address: 'Silicon Valley, California, United States of America'),
+  Business(
+      name: 'ABCNO',
+      category: 'DEF',
+      phone: '1231231231',
+      address: 'ADSLKFLAKDF'),
+  Business(
+      name: 'DEFLM',
+      category: 'DEF',
+      phone: '1231231231',
+      address: 'ADSLKFLAKDF'),
+  Business(
+      name: 'GHIJK',
+      category: 'DEF',
+      phone: '1231231231',
+      address: 'ADSLKFLAKDF'),
+];
